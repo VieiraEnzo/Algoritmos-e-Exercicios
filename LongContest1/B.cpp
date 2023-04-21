91f00bb07a2f5fc6c20d60b4bfe1b16177f98a17
Enzo <vieira.enzof@gmail.com>
2023-04-20 21:51:03 -0300

    feat: A and B solved Contest 1

--- LongContest1/B.cpp
+++ LongContest1/B.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    int n , array[10005];
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> array[i];
+    }
+
+    for(int i = n - 1; i >= 0; i--){
+        cout << array[i] << ' ';
+    }
 }
