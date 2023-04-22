7cf2471fbab423127a2d404c197c7156bb05df01
Enzo <vieira.enzof@gmail.com>
2023-04-22 19:27:27 -0300

    feat: test file + J resolved

--- LongContest1/I.cpp
+++ LongContest1/I.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
-    
+    string problem;
+    cin >> problem;
+    int Sz = problem.size();
+    long long int sum = 0;
+    for(int i = 0; i < Sz; i ++){
+        sum += (problem[i] - 'A' + 1) * pow(26, (Sz - i - 1));
+    }
+    cout << sum;
 }
--- LongContest1/J.cpp
+++ LongContest1/J.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(){
-    
+    int t,n,s,r;
+    int array[50];
+    cin >> t;
+
+    for(int i = 0 ; i < t; i ++){
+        cin >> n >> s >> r;
+        cout << s - r << ' ';
+        int loops = 1;
+        while (true)
+        {   
+            if(r % (n-loops) == 0){
+                for(int i =0; i < n-loops; i++){
+                    cout << r/(n-loops) << ' ';
+                }
+                break;
+            } 
+            else{
+                cout << r/(n-loops) << ' ';
+                r -= r/(n-loops);
+                loops ++;
+            }
+        }
+        cout << endl;
+    }
 }
