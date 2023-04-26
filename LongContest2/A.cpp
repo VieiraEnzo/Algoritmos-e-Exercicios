b2e5e5e08780ee46442c74340df9376dc0416dc1
Enzo <vieira.enzof@gmail.com>
2023-04-25 23:21:42 -0300

    feat: B solved

--- LongContest2/A.cpp
+++ LongContest2/A.cpp
@@ -7,21 +7,20 @@ int main(){
     cin >> n;
     for(int i=0; i < n ;i++){
 
-        int numer_interrogation = 1;
-        int interrogation_front = 1;
+        long long int numer_interrogation = 1;
+        long long int interrogation_front = 1;
         cin >> mask;
 
         if (mask[0] == '0'){cout << 0; continue;}
         else if (mask[0] == '?'){ interrogation_front = 9;}
-        else{}
 
         for(int i=1; i < mask.size();i++){
             if (mask[i] == '?'){
                 numer_interrogation ++;
             }
         }
-        if(numer_interrogation > 1){numer_interrogation = pow(10,(numer_interrogation -1));}
-        cout << numer_interrogation * interrogation_front;
+        if(numer_interrogation > 1){numer_interrogation = (long long)pow(10,(numer_interrogation -1));}
+        cout << numer_interrogation * interrogation_front << endl;
 
     }
 }
