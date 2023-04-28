8b33ad1399089e3955dc1c3c46c3f399e6d40b4a
Enzo <vieira.enzof@gmail.com>
2023-04-27 21:36:33 -0300

    feat: A finished and E too

--- LongContest2/A.cpp
+++ LongContest2/A.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long int elevado(long long int a, long long int b){
+    long long int elevado = 1;
+    if(b == 0){return 1;}
+    for(int i = 0; i < b; i++){
+        elevado *= a;
+    }
+    return elevado;
+}
+
 int main(){
+
     int n;
     string mask;
     cin >> n;
@@ -11,7 +21,7 @@ int main(){
         long long int interrogation_front = 1;
         cin >> mask;
 
-        if (mask[0] == '0'){cout << 0; continue;}
+        if (mask[0] == '0'){cout << 0 << endl; continue;}
         else if (mask[0] == '?'){ interrogation_front = 9;}
 
         for(int i=1; i < mask.size();i++){
@@ -19,7 +29,7 @@ int main(){
                 numer_interrogation ++;
             }
         }
-        if(numer_interrogation > 1){numer_interrogation = (long long)pow(10,(numer_interrogation -1));}
+        if(numer_interrogation > 1){numer_interrogation = elevado(10,(numer_interrogation -1));}
         cout << numer_interrogation * interrogation_front << endl;
 
     }
