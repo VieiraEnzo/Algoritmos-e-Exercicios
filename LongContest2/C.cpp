50de9f3f7d167fe29dcc417eab48a3e623347d64
Enzo <vieira.enzof@gmail.com>
2023-05-01 14:59:05 -0300

    fix: H broken, C solved

--- LongContest2/C.cpp
+++ LongContest2/C.cpp
@@ -1,33 +1,61 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 int main(){
     int t,n;
     cin >> t;
+    vector<int> vec;
+    vector<int> answer;
+    int num;
 
     for(int init = 0; init < t; init ++){
 
         cin >>n;
-        long long int b[n-1];
-        long long int answer[n];
+        answer.clear();
+        vec.clear();
         for(int i=0; i<n-1; i++){
-            cin >> b[i];
+            cin >> num;
+            vec.push_back(num);
         }
 
-        for(int i=0; i<n-2; i++){
-            if(b[i]<b[i+1]){
-                answer[i+1] = b[i];
-            }
-            if(b[i]==b[i+1]){
-                answer[i] = b[i];
-                answer[i+1] = b[i+1];
-                
-            }
-            else{
-                answer[i] = b[i];
-                answer[i+1] = 0;
-            }
-        }
+        bool first = true;
+        for(int i = 0; i < n-2; i ++){
 
+            if(vec[i] < vec[i+1] && i == 0){
+                answer.push_back(vec[i]);
+                answer.push_back(0);
+                first = false;
+            }
+
+            else if(vec[i] < vec[i+1] && first && i != 0){
+                answer.push_back(0);
+                answer.push_back(vec[i]);
+                first = false;
+            }
+
+            else if(vec[i] < vec[i+1] && !first){
+                answer.push_back(vec[i]);
+            }
+
+            else if(vec[i] == vec[i+1]){
+                answer.push_back(vec[i]);
+            }
+
+            else if(vec[i] > vec[i+1]){
+                answer.push_back(vec[i]);
+            }
+            
+        }
+        answer.push_back(vec[vec.size() - 1]);
+        for(int i = 0; i < n - answer.size(); i ++){
+            answer.push_back(0);
+        }
+        // se diferente do tamanho adicionar 0 no final
+        for(int i=0;i<n;i++){
+            cout << answer[i] << ' ';
+        }
+        cout << endl;
+        
     }
 }
--- LongContest2/H.cpp
+++ LongContest2/H.cpp
@@ -1,19 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool is_paranoid(string number);
+bool is_paranoid_front(string number);
+bool is_paranoid_secound(string number);
+
+bool is_paranoid(string number){
+    if(number.size() == 1){
+        return true;
+    }
+    if(number.size() == 2){
+        if(number[0] == number[1]){return false;}
+        return true;
+    }
+    bool all_equal = true;
+    for(int i=0;i<number.size();i++){
+        if(i != i){
+            all_equal = false;
+            break;
+        }
+    }
+    if(all_equal){return false}
+    else{return is_paranoid_front(number) || is_paranoid_secound(number);}
+}
+
+bool is_paranoid_front(string number){
+    string substring;
+    if(number.size() == 3){
+        if(number[0] == number[1]){return false;}
+        substring.push_back(number[1]);
+    }
+
+    int prev = 0;
+    for(int i=2;i < number.size() -1; i=i+2){
+        if(number[i] != number[prev] && number[i+1] != number[prev+1]){
+            substring.push_back(number[i+1]);
+            prev = i + 2;
+        }
+    }
+
+
+
+    if(number.size() % 2 != 0){substring.push_back(number[number.size()-1]);}
+    return is_paranoid(substring);
+}
+
+bool is_paranoid_secound(string number){
+    string substring;
+    if(number.size() == 3){
+        if(number[1] == number[2]){return false;}
+        substring.push_back(number[0]);
+        substring.push_back(number[2]);
+    }
+    if(number.size() == 4){
+        if(number[1] == number[2]){return false;}
+        substring.push_back(number[0]);
+        substring.push_back(number[2]);
+    }
+
+    int prev = 1;
+    substring.push_back(number[0]);
+    for(int i=3;i < number.size() -1; i=i+2){
+
+        if(number[i] != number[prev] && number[i+1] != number[prev+1]){
+            substring.push_back(number[i+1]);
+            prev = i + 2;
+        }
+    }
+    if(number.size() % 2 == 0){substring.push_back(number[number.size()-1]);}
+
+    return is_paranoid(substring);
+}
+
+
 int main(){
     long long int t;
     cin >> t;
     long long size;
-    string numbers;
-    
+    string number;
     for(int init = 0;init < t; init++){
-        cin >> size >> numbers;
+        cin >> size >> number;
 
 
-    
-    cout << size + 
+        for(int i =0; i < number.size() -1; i++){
+            for(int j=i+1;j < number.size();j++){
+                if(is_paranoid(number.substr(i,j-i+1))){
+                    size ++;
+                } 
+            }
+        }
+        cout << size << endl;
+
     }
-
-
 }
