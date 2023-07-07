5f133ac27cc82228b4cd40979f59920a1ee3facd
Enzo <vieira.enzof@gmail.com>
2023-07-07 15:00:29 -0300

    feat: ShortContest3 preparing

--- LongContest5/A.cpp
+++ LongContest5/A.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
-    
+    int x,y,a,b;
+    cin >> x >> y >> a >> b;
+    //cout <<(x-a)*(y-b) << endl;
+    int sum = 0;
+    for(int i =a; i <= x; i++){
+        for(int j=b; j < i && j <= y; j++){
+            sum++;
+        }
+    }
+    cout << sum << endl;
+    for(int i =a; i <= x; i++){
+        for(int j=b; j < i && j <= y; j++){
+            cout << i << ' ' << j << endl;
+        }
+    }
 }
--- LongContest5/C.cpp
+++ LongContest5/C.cpp
@@ -10,17 +10,19 @@ int main(){
         cin >> temp.first >> temp.second;
         points.push_back(temp);
     }
+
+    //PROBLEMA: Algortimo n^3;
+    //OPÇÃO: Ordenar e fazer busca binária
     int count = 0;
-    for(int i = 0;i < n -2; i++){
-        for(int j = i+1;j < n -1; j++){
-            for(int k = j+1;k < n; k++){
-                if((points[i].first - points[j].first) == 0 || (points[i].first - points[k].first) == 0){continue;}
-                if((points[i].second - points[j].second)/(points[i].first - points[j].first) == (points[i].second - points[k].second)/(points[i].first - points[k].first)){
-                    // cout << points[i].first << " "<< points[j].first << " "<< points[k].first << " " << endl;
+    for(int i = 0;i < n -1; i++){
+        for(int j = i+1;j < n; j++){
+            for(int k = 0; k < n; k ++){
+                if(k == i || k == j){continue;}
+                if((float)(points[i].first + points[j].first)/2 == points[k].first && (float)(points[i].second + points[j].second)/2 == points[k].second){
                     count++;
                 }
-            }
+            }      
         }
     }
     cout << count;
-}
+    }
