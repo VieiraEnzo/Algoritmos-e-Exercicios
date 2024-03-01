ff99f14fade891f122d933a3a3e49df1782860ee
Enzo <vieira.enzof@gmail.com>
2024-03-01 20:46:18 -0300

    template

--- template.cpp
+++ template.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 #pragma GCC optimize("pragv")
+
 using namespace std;
+using namespace std;
+using namespace __gnu_pbds;
+
 #define endl "\n"
 #define fastio cin.tie(nullptr), ios_base::sync_with_stdio(false)
+
 typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int main(){
     fastio;
