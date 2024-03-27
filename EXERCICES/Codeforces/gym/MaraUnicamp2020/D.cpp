c9ce02362bad370103292e3548bb397813d9d46d
VieiraEnzo <vieira.enzof@gmail.com>
2024-03-26 21:14:56 -0300

    novas historias e coisas lindas

---
+++ EXERCICES/Codeforces/gym/MaraUnicamp2020/D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//DP[n][m] : menor numero de talismas, pegando no dia n
+// com m talismas;
+//pegar -> melhor resposta: DP[n][m]
+
+int main(){
+
+}
