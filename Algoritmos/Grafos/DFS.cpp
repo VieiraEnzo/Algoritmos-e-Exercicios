aaae48df868d225bfbc11d8307e177ebffb6bc5c
VieiraEnzo <vieira.enzof@gmail.com>
2024-04-04 11:22:07 -0300

    opa

--- Algoritmos/Grafos/DFS.cpp
+++ Algoritmos/Grafos/DFS.cpp
@@ -14,3 +14,25 @@ template<typename T > struct DFS{
     }   
 
 };
+
+//iterativo
+
+template<typename T> struct  DfsIterativo
+{
+    while (!pilha.empty())
+    {
+        int v = pilha.top();
+        pilha.top();
+
+        if(!vis[v]){
+
+            vis[v] = true;
+
+            for(int next : grafo[v]){
+                if(vis[next]) continue;
+                pilha.push(next);
+            }
+
+        }
+    }   
+};
--- dfsIt.cpp
+++ dfsIt.cpp
@@ -4,6 +4,36 @@ using namespace std;
 
 int main(){
 
+    int n, m; cin >> n >> m;
+    vector<vector<int>> grafo(n+1);
+    vector<bool> vis(n+1);
+
+    for(int i = 0; i < m; i++){
+        int a,b; cin >> a >> b;
+        grafo[a].push_back(b);
+        grafo[b].push_back(a);
+    }
+
+    stack<int> pilha;
+    pilha.push(3);
+
+    while (!pilha.empty())
+    {
+        int v = pilha.top();
+        pilha.top();
+
+        if(!vis[v]){
+
+            vis[v] = true;
+
+            for(int next : grafo[v]){
+                if(vis[next]) continue;
+                pilha.push(next);
+            }
+
+        }
+    }
     
+
 
 }
