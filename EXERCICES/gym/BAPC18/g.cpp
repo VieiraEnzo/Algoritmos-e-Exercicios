21eda3a13ffc8f45b0e7b2f5d8cc57cab107581b
VieiraEnzo <vieira.enzof@gmail.com>
2024-05-08 19:40:06 -0300

    Upsolved

--- EXERCICES/gym/BAPC18/g.cpp
+++ EXERCICES/gym/BAPC18/g.cpp
@@ -1,25 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-string s;
-vector<int> v(3);
-vector<char> oi = {'A', 'B', 'C'};
+int n; string s;
+vector<int> oi = {'A', 'B', 'C'};
 
-int solve(int l, int r){
-    string comp;
-    for(int i = 0; i < v[oi[0]- 'A']; i++)comp.push_back(oi[0]);
-    for(int i = 0; i < v[oi[1]- 'A']; i++)comp.push_back(oi[1]);
-    for(int i = 0; i < v[oi[2]- 'A']; i++)comp.push_back(oi[2]);
+int solve(){
+    map<char,int> quant;
+    map<char,int> ans;
+    
 
-    int ans = 0;
-    for(int i = l; i < r; i++){
-        if(comp[i - l] != s[i]){
-            ans++;
-        }
+
+    for(int i = 0; i < n; i++){
+        quant[s[i]]++;
     }
-    return ans;
+
+    for(int i = 0; i < quant[oi[0]]; i++){
+        if(s[i] != oi[0]) ans[oi[0]]++;
+    }
+    for(int i = quant[oi[0]]; i < quant[oi[0]] + quant[oi[1]]; i++){
+        if(s[i] != oi[1])ans[oi[1]]++;
+    }
+    for(int i = quant[oi[0]] + quant[oi[1]]; i <quant[oi[0]] + quant[oi[1]] +  quant[oi[2]]; i++){
+        if(s[i] != oi[2]) ans[oi[2]]++;
+    }
+
+    // cout << "2" << endl;
+
+
+    int resp = ans[oi[0]] + ans[oi[1]] + ans[oi[2]];
+
+    for(int l = 1; l <= n; l++){
+        // cout << resp << endl;
+        int r = l + n;
+        int la = l;
+        int ra = la + quant[oi[0]] - 1;
+        int lb = ra + 1;
+        int rb = lb + quant[oi[1]] - 1;
+        int lc = rb + 1;
+        int rc = lc + quant[oi[2]] - 1;
+        if(s[la-1] == oi[0]) ans[oi[0]]++;
+        if(s[ra] == oi[0]) ans[oi[0]]--;
+        if(s[lb-1] == oi[1]) ans[oi[1]]++;
+        if(s[rb] == oi[1]) ans[oi[1]]--;
+        if(s[lc-1] == oi[2]) ans[oi[2]]++;
+        if(s[rc] == oi[2]) ans[oi[2]]--;
+        resp = min(resp, ans[oi[0]] + ans[oi[1]] + ans[oi[2]]);
+        
+    }
+
+    return resp;
 }
+
 
 
 int main(){
@@ -27,18 +58,13 @@ int main(){
     cin >> s;
     s.append(s);
 
-    for(int i = 0; i < n; i++) v[s[i] - 'A']++;
+    int resp = 1e9;
 
-    int ans = 1e9;
-    for(int i = 0; i < n; i++){
-        // for(int j = i; j < i+n; j++){cout << s[j];}
-        // cout << endl;
-        sort(oi.begin(), oi.end());
-        do{
-            ans = min(solve(i, i+n), ans);
-            
-        }while (next_permutation(oi.begin(), oi.end()));
-    }
-    
-    cout << ans << endl;
+    do{
+        resp = min(solve(), resp);
+    }while(next_permutation(oi.begin(), oi.end()));
+
+
+    cout << resp << endl;
+
 }
