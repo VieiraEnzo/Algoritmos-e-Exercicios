f137f3b88fa11afd5764daf6f9d8344d71deb680
VieiraEnzo <vieira.enzof@gmail.com>
2024-05-20 16:50:18 -0300

    A E ta foda

--- EXERCICES/Codeforces/Divs/Div3/943/E.cpp
+++ EXERCICES/Codeforces/Divs/Div3/943/E.cpp
@@ -11,6 +11,14 @@ typedef unsigned long long ull;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 
+pair<int,int> operator*(pair<int,int> a, int b){
+    return {a.first * b, a.second * b};
+}
+pair<int,int> operator+(pair<int,int> a,pair<int,int> b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+
 void solve(){
     int n; cin >> n;
    // 1 ... n
@@ -18,8 +26,20 @@ void solve(){
    // . . . .
    // . . . .
    // n ... i
+   int i = 0;
+   vector<pair<int,int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+   pair<int,int> init = {1,1};
+
+   do
+   {
+        init = init + dir[i]*(n);
+        i++;i%=4;
+        n/=2;
+        cout << init.first << " " << init.second << "\n";
+   }while (n > 0);
    
-    
+
+    cout << "\n";
 }
 
 int main(){
