5bf209630dce1563f51373b13c56070a6fa089bf
VieiraEnzo <vieira.enzof@gmail.com>
2024-06-04 13:16:37 -0300

    bom dia

--- EXERCICES/Atcoder/ABC/282/D.cpp
+++ EXERCICES/Atcoder/ABC/282/D.cpp
@@ -25,13 +25,14 @@ int maxn = 2e5 + 10;
 vector<int> color(maxn, -1);
 vector<vector<int>> grafo(maxn);
 
-pair<int, pair<int,int>> bfs(int v){
+pair<int, vector<int>> bfs(int v){
     
     queue<int> q;
     q.push(v);
     color[v] = 0;
     bool Bipart= true;
-    int numV = 1, numA = 0;
+    vector<int> num(3);
+    num[0]++;
 
     while (!q.empty())
     {
@@ -39,17 +40,18 @@ pair<int, pair<int,int>> bfs(int v){
         q.pop();
 
         for(auto a : grafo[k]){
-            numA++; //COntando quantas arestas
-            if(color[a] == color[k]) Bipart = false;
+            num[2]++;
+            if(color[a] == color[k]) {Bipart = false; continue;}
             if(color[a] != -1) continue;
             color[a] = color[k]^1;
-            numV++;
+            num[color[a]]++;            
             q.push(a);
         }
     }
 
-    if(Bipart) return {1, {numV, numA/2}};
-    return {0, {numV, numA/2}};
+    num[2]/=2;
+    if(Bipart) return {1, num};
+    return {0, num};
 }   
 
 
@@ -64,25 +66,27 @@ signed main(){
     }
 
 
-    vector<pair<int,int>> compConex;
+    vector<vector<int>> compConex;
     for(int i = 1; i <= n; i++){
         if(color[i] == -1){
             auto num = bfs(i);
             if(num.first == 1){
                 compConex.push_back(num.second);
+            }else{
+                cout << 0 << "\n"; return 0;
             }
         }
     }
     
-    // pr(compConex);
+    pr(compConex);
 
     int ans = 0;
     int sumVert = 0;
     for(int i = 0; i < (int)compConex.size(); i++){
-        ans += sumVert * compConex[i].first; //Dessa componente com todas as outras
-        ans += ((compConex[i].first/2) *  ((compConex[i].first+1)/2)) - compConex[i].second; //Vertices Pretos com os brancos
-        sumVert += compConex[i].first;
-        // pr(sumVert, ans);
+        ans += sumVert * (compConex[i][0] + compConex[i][1]); //Dessa componente com todas as outras
+        ans += (compConex[i][0] *  compConex[i][1]) - compConex[i][2] ; //Vertices Pretos com os brancos
+        sumVert += (compConex[i][0] + compConex[i][1]);
+        pr(sumVert, ans);
     }
 
     cout << ans << "\n";
