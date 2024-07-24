c194fc565cf35bb8a041de81bb0d2ca577934670
VieiraEnzo <vieira.enzof@gmail.com>
2024-07-24 15:20:18 -0300

    morte

--- a.cpp
+++ a.cpp
@@ -1,39 +1,128 @@
 #include <bits/stdc++.h>
-typedef long long ll;
 using namespace std;
+typedef long long ll;
 
-ll maxn = 1e5 + 10;
-vector<vector<ll>> grafo(maxn);
-vector<ll> marc(maxn);
-ll count = 0;
-vector<ll> leaves;
-ll h;
+#ifdef local
+    #include "/home/enzo/Documents/Prog/OBItraining/debug.h"
+    #define pr(...) debug(#__VA_ARGS__, __VA_ARGS__)
+    #define prs(...) debug_nameless(__VA_ARGS__)
+#else
+    #define pr(...) 69
+    #define prs(...) 69
+#endif
 
-void dfs(ll v){ 
-    marc[v] = 1;
-    if(grafo[v].size() == 1){leaves.push_back(v);}
-    for(auto viz : grafo[v]){
-        if(!marc[viz]) {dfs(viz);}
+char bestType;
+
+vector<string> MainHand;
+vector<string> otherHand;
+
+void getbest(set<int> &A, int capacidade, char tipo){
+    
+    while(!A.empty() && capacidade > 0){
+        capacidade--;
+        int res = *(--A.end());
+        A.erase(--A.end());
+        string morte = to_string(res) + tipo;
+        MainHand.push_back(morte);  
+    }
+
+    while(!A.empty()){
+        int res = *(--A.end());
+        A.erase(--A.end());
+        string morte = to_string(res) + tipo;
+        otherHand.push_back(morte);  
     }
 }
 
+void getbest2(set<int> &A, int capacidade, char tipo){
+
+    int tamanho = A.size();
+    
+    while(A.size() > (tamanho+1)/2 && capacidade > 0){
+        capacidade--;
+        int res = *(--A.end());
+        A.erase(--A.end());
+        string morte = to_string(res) + tipo;
+        MainHand.push_back(morte);  
+    }
+
+    while(!A.empty()){
+        int res = *(--A.end());
+        A.erase(--A.end());
+        string morte = to_string(res) + tipo;
+        otherHand.push_back(morte);  
+    }
+}
+
+string getBestCard(string S){
+
+    for(auto i = otherHand.begin(); i != otherHand.end(); i++){
+        string card = *i;
+        if(card[2] == bestType) continue;
+        if(card[2] == S[2] && card[1] < S[1]){
+            otherHand.erase(i);
+            return card;
+        }
+    }
+
+    for(auto i = otherHand.begin(); i != otherHand.end(); i++){
+        string card = *i;
+        if(card[2] != bestType || card[1] > S[2]) continue;
+        otherHand.erase(i);
+        return card;
+    }
+
+    return "morte";
+}
+
+void solve(){
+    MainHand.clear();
+    otherHand.clear();
+
+    int n; cin >> n;
+    char s; cin >> s;
+    bestType = s;
+    set<int> C,D,H,S;
+
+    for(int i = 0; i < 2*n; i++){
+        string k; cin >> k;
+        pr(k);
+        string morte; morte.push_back(k[0]);
+        if(k[1] == 'C') C.insert(stoi(morte));
+        if(k[1] == 'D') D.insert(stoi(morte));
+        if(k[1] == 'H') H.insert(stoi(morte));
+        if(k[1] == 'S') S.insert(stoi(morte));
+    }
+
+    pr(C,D,H,S);
+
+    if(s == 'C') getbest(C, n, 'C');
+    else if(s == 'D') getbest(D, n, 'D');
+    else if(s == 'H') getbest(H, n, 'H');
+    else getbest(S, n, 'S');
+
+    getbest2(C, n - MainHand.size(), 'C');
+    getbest2(D, n - MainHand.size(), 'D');
+    getbest2(H, n - MainHand.size(), 'H');
+    getbest2(S, n - MainHand.size(), 'S');
+
+    sort(MainHand.begin(), MainHand.end());
+    sort(otherHand.begin(), otherHand.end());
+
+    pr(MainHand);
+    pr(otherHand);
+
+    if(MainHand.size() != otherHand.size()) {cout << "IMPOSSIBLE\n"; return;}
+
+    for(auto card : MainHand){
+        string other = getBestCard(card);
+        cout << other << " " << card << "\n";
+    }
+
+}
+
 int main(){
-    cin.tie(0); ios_base::sync_with_stdio(0);
-    ll n; cin >> n >> h;
-    for(ll i = 0; i < n-1; i++){
-        ll a,b; cin >> a >> b;
-        grafo[a].push_back(b);
-        grafo[b].push_back(a);
-    }
-
-    ll init = h;
-    if(grafo[h].size() == 1) init = grafo[h].back(); 
-    dfs(init);
-
-    cout << (leaves.size()+1)/2 << "\n";
-    for(ll i = 0; i < leaves.size()/2; i++){
-        cout << leaves[i] << " " << leaves[i + leaves.size()/2 + leaves.size()%2] << "\n";
-    }
- 
-    if(leaves.size()%2 == 1){ cout << leaves[leaves.size()/2 ] << " " << init << "\n";}
-}   
+    cin.tie(0), ios_base::sync_with_stdio(0);
+    int t; cin >> t;
+    while(t--) solve();
+}
