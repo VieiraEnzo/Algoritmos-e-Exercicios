7ef627f8f4447ecf42244486805d0f670201f39c
VieiraEnzo <vieira.enzof@gmail.com>
2024-08-25 23:36:15 -0300

    to codando ainda

--- a.cpp
+++ a.cpp
@@ -22,7 +22,11 @@ typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 
 void solve(){
-
+    int n; cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 1; i < n; i++) cout << arr[i] << " ";
+    cout << arr[0] << "\n";
 }
 
 signed main(){
