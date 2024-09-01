34920452fa03112f8206fbd8be066adad9838dcf
VieiraEnzo <vieira.enzof@gmail.com>
2024-09-01 16:39:35 -0300

    solved, new templateCF

--- a.cpp
+++ a.cpp
@@ -17,18 +17,63 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 
 typedef long long ll;
-typedef unsigned long long ull;
 typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+const int mod = 1e9 + 7;
+
+int add(int a, int b) { a += b; if (a >= mod) a -= mod; return a; }
+int sub(int a, int b) { a -= b; if (a < 0) a += mod; return a; }
+int mul(int a, int b) { return 1ll * a * b % mod; }
+int Pow(int a, int b) { int ans = 1; for (; b; b >>= 1, a = mul(a, a)) if (b & 1) ans = mul(ans, a); return ans; }
 
 void solve(){
-    int n; cin >> n;
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++) cin >> arr[i];
-    for(int i = 1; i < n; i++) cout << arr[i] << " ";
-    cout << arr[0] << "\n";
+    int n, k; cin >> n >> k;
+    multiset<int> s;
+    for(int i = 0; i < n; i++){
+        int tmp; cin >> tmp;
+        s.insert(tmp);
+    }
+
+    while(*s.begin() != *prev(s.end())){
+        int x = *s.begin();
+        int y = *prev(s.end());
+        s.erase(prev(s.end()));
+
+        y %= x;
+        if(y == 0){
+            s.insert(x);
+        }else{
+            s.insert(y);
+        }
+    }
+
+    int minE = *s.begin();
+    if(n == 1){
+        if(k-1 < minE){
+            cout << k-1 << "\n";
+        }else{
+            cout << k << "\n";
+        }
+        return;
+    }
+    n--;
+
+    if(minE == 1){
+        cout << k + n << "\n";
+        return;
+    }
+
+
+    int q = (k-1) /(minE-1);
+
+    cout << k + min(n,q) << "\n";
+
+//mex0
+
 }
 
+//mex 4
+//0 2 4 6 8 10
+//
 signed main(){
     fastio;
     int t; cin >> t;
--- templateCF.cpp
+++ templateCF.cpp
@@ -17,9 +17,13 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 
 typedef long long ll;
-typedef unsigned long long ull;
 typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+const int mod = 1e9 + 7;
+
+int add(int a, int b) { a += b; if (a >= mod) a -= mod; return a; }
+int sub(int a, int b) { a -= b; if (a < 0) a += mod; return a; }
+int mul(int a, int b) { return 1ll * a * b % mod; }
+int Pow(int a, int b) { int ans = 1; for (; b; b >>= 1, a = mul(a, a)) if (b & 1) ans = mul(ans, a); return ans; }
 
 void solve(){
 
