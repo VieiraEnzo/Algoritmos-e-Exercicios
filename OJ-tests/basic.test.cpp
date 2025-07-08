f149fdc20ade9f544c5f7d8c00baffa52dee0778
VieiraEnzo <vieira.enzof@gmail.com>
2025-07-08 13:27:21 -0300

    teste se ta funfando

---
+++ OJ-tests/basic.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b; cin >> a >> b;
+    std::cout << a + b << std::endl;
+    return 0;
+}
