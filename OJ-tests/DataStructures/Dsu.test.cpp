6eed0725a32833b9a2d71773d37d2bf96c9b2d7b
VieiraEnzo <vieira.enzof@gmail.com>
2025-07-08 14:22:38 -0300

    Primeiro teste válido

---
+++ OJ-tests/DataStructures/Dsu.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../Algoritmos/DataStructures/DSU.h"
+
+int main(){
+    int n; cin >> n;
+    int q; cin >> q;
+
+    DSU dsu(n);
+
+    while(q--){
+        int t; cin >> t;
+        int a,b; cin >> a >> b;
+        if(t == 0){
+            dsu.uu(a,b);
+        }else{
+            cout << (dsu.find(a) == dsu.find(b)) << "\n";
+        }
+    }
+}
